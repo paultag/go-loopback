78c50244eafa9456c0d0443d2e4d4233d65389d4
Paul R. Tagliamonte <paultag@gmail.com>
2021-03-29 21:20:46 -0400

    Do some cleanup for the latest Go standards.

--- loopback.go
+++ loopback.go
@@ -1,3 +1,23 @@
+// {{{ Copyright (c) Paul R. Tagliamonte <paultag@gmail.com> 2017-2021
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE. }}}
+
 package loopback
 
 /*
@@ -7,45 +27,66 @@ import "C"
 
 import (
 	"fmt"
-	"log"
 	"os"
 	"syscall"
 )
 
 type (
+	// LoopInfo64 is the 64 bit loop info variant. This is used throughout
+	// this codebase. If you need to use LoopInfo due to a 32 bit program,
+	// please open a bug on this library.
 	LoopInfo64 struct {
-		loDevice           uint64 /* ioctl r/o */
-		loInode            uint64 /* ioctl r/o */
-		loRdevice          uint64 /* ioctl r/o */
-		loOffset           uint64
-		loSizelimit        uint64 /* bytes, 0 == max available */
-		loNumber           uint32 /* ioctl r/o */
-		loEncrypt_type     uint32
-		loEncrypt_key_size uint32 /* ioctl w/o */
-		loFlags            uint32 /* ioctl r/o */
-		loFileName         [LoNameSize]uint8
-		loCryptName        [LoNameSize]uint8
-		loEncryptKey       [LoKeySize]uint8 /* ioctl w/o */
-		loInit             [2]uint64
+		loDevice         uint64 /* ioctl r/o */
+		loInode          uint64 /* ioctl r/o */
+		loRdevice        uint64 /* ioctl r/o */
+		loOffset         uint64
+		loSizelimit      uint64 /* bytes, 0 == max available */
+		loNumber         uint32 /* ioctl r/o */
+		loEncryptType    uint32
+		loEncryptKeySize uint32 /* ioctl w/o */
+		loFlags          uint32 /* ioctl r/o */
+		loFileName       [LoNameSize]uint8
+		loCryptName      [LoNameSize]uint8
+		loEncryptKey     [LoKeySize]uint8 /* ioctl w/o */
+		loInit           [2]uint64
 	}
 )
 
-// IOCTL consts
 const (
-	LoopSetFd       = C.LOOP_SET_FD
-	LoopCtlGetFree  = C.LOOP_CTL_GET_FREE
+	// LoopSetFd will associate the loop device with the open file
+	LoopSetFd = C.LOOP_SET_FD
+
+	// LoopCtlGetFree will allocate or find a free loop device for use.
+	LoopCtlGetFree = C.LOOP_CTL_GET_FREE
+
+	// LoopGetStatus64 will get the status of the loop device.
 	LoopGetStatus64 = C.LOOP_GET_STATUS64
+
+	// LoopSetStatus64 will set the status of the loop device.
 	LoopSetStatus64 = C.LOOP_SET_STATUS64
-	LoopClrFd       = C.LOOP_CLR_FD
+
+	// LoopClrFd will disassociate the loop device from any file descriptor.
+	LoopClrFd = C.LOOP_CLR_FD
+
+	// LoopSetCapacity will resize a live loop device.
 	LoopSetCapacity = C.LOOP_SET_CAPACITY
 )
 
 const (
+	// LoFlagsAutoClear will instruct the kernel to autodestruct on last close.
 	LoFlagsAutoClear = C.LO_FLAGS_AUTOCLEAR
-	LoFlagsReadOnly  = C.LO_FLAGS_READ_ONLY
-	LoFlagsPartScan  = C.LO_FLAGS_PARTSCAN
-	LoKeySize        = C.LO_KEY_SIZE
-	LoNameSize       = C.LO_NAME_SIZE
+
+	// LoFlagsReadOnly requests the loopback device be read-only.
+	LoFlagsReadOnly = C.LO_FLAGS_READ_ONLY
+
+	// LoFlagsPartScan will allow automatic partition scanning.
+	LoFlagsPartScan = C.LO_FLAGS_PARTSCAN
+
+	// // LoKeySize is the length of the encryption key
+	// LoKeySize  = C.LO_KEY_SIZE
+	//
+	// // LoNameSize is the length of the file name.
+	// LoNameSize = C.LO_NAME_SIZE
 )
 
 // syscalls will return an errno type (which implements error) for all calls,
@@ -57,9 +98,13 @@ func errnoIsErr(err error) error {
 	return nil
 }
 
-// Given a handle to a Loopback device (such as /dev/loop0), and a handle
-// to the image to loop mount (such as a squashfs or ext4fs image), preform
-// the required call to loop the image to the provided block device.
+// Loop will, when given a handle to a Loopback device (such as /dev/loop0),
+// and a handle to the fs image to loop mount (such as a squashfs or ext4fs
+// image), preform the required call to loop the image to the provided block
+// device.
+//
+// The first argument (loopbackDevice) can be obtained using
+// loopback.NextLoopDevice, if one is not known in advance.
 func Loop(loopbackDevice, image *os.File) error {
 	_, _, err := syscall.Syscall(
 		syscall.SYS_IOCTL,
@@ -70,17 +115,18 @@ func Loop(loopbackDevice, image *os.File) error {
 	return errnoIsErr(err)
 }
 
-// Given a handle to the Loopback device (such as /dev/loop0), preform the
-// required call to the image to unloop the file.
+// Unloop will, given a handle to the Loopback device (such as /dev/loop0),
+// preform the required call to the image to unloop the image mounted at
+// that location.
 func Unloop(loopbackDevice *os.File) error {
 	_, _, err := syscall.Syscall(syscall.SYS_IOCTL, loopbackDevice.Fd(), LoopClrFd, 0)
 	return errnoIsErr(err)
 }
 
-// Get the next loopback device that isn't used. Under the hood this will ask
-// loop-control for the LOOP_CTL_GET_FREE value, and interpolate that into
-// the conventional GNU/Linux naming scheme for loopback devices, and os.Open
-// that path.
+// NextLoopDevice will return the next loopback device that isn't used. Under
+// the hood this will ask loop-control for the LOOP_CTL_GET_FREE value, and
+// interpolate that into the conventional GNU/Linux naming scheme for loopback
+// devices, and os.Open that path.
 func NextLoopDevice() (*os.File, error) {
 	loopInt, err := nextUnallocatedLoop()
 	if err != nil {
@@ -89,8 +135,8 @@ func NextLoopDevice() (*os.File, error) {
 	return os.Open(fmt.Sprintf("/dev/loop%d", loopInt))
 }
 
-// Return the integer of the next loopback device we can use by calling
-// loop-control with the LOOP_CTL_GET_FREE ioctl.
+// nextUnallocatedLoop will return the integer of the next loopback device we
+// can use by calling loop-control with the LOOP_CTL_GET_FREE ioctl.
 func nextUnallocatedLoop() (int, error) {
 	fd, err := os.OpenFile("/dev/loop-control", os.O_RDONLY, 0644)
 	if err != nil {
@@ -101,8 +147,8 @@ func nextUnallocatedLoop() (int, error) {
 	return int(index), errnoIsErr(err)
 }
 
-// Get the next loopback device that isn't used, loopback the image,
-// and mount the loopback device to the target.
+// MountImage will get the next loopback device that isn't used, loopback the
+// provided image, and mount the loopback device to the target.
 func MountImage(
 	image *os.File,
 
@@ -133,3 +179,5 @@ func MountImage(
 		lo.Close()
 	}, nil
 }
+
+// vim: foldmethod=marker
